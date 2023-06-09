ec80fd0f9618962eee2ec2a5706eca6c7c455349
shoaib-ali-asif <eagleshebi@gmail.com>
2023-06-09 12:46:19 +0500

    changed multiple things

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
-const formElement = document.getElementById("form")
-const nameElement = document.getElementById("name")
-const emailElement = document.getElementById("email")
-const passwordElement = document.getElementById("password")
-const confirmPasswordElement = document.getElementById("password-confirmation")
+const formElement = document.getElementById("form");
+const errorElement = document.getElementById("error");
 
+formElement.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nameElement = document.getElementById("name");
+  const emailElement = document.getElementById("email");
+
+
+  let nameValue = nameElement.value;
+  let emailValue = emailElement.value;
+  
+  errorElement.innerText = "";
+  nameElement.classList.remove("redborder");
+  emailElement.classList.remove("redborder");
+  
+  if (nameValue == "") {
+    errorElement.innerText = "Name is required!";
+    nameElement.classList.add("redborder");
+  }
+  if (emailValue == "") {
+    errorElement.innerText = "Enter your email!";
+    emailElement.classList.add("redborder");
+  }
+    console.log("good to go");
+  
+});
