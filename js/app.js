98f746e6b3290f3f62b8e80edc959cf2b587d510
shoaib-ali-asif <eagleshebi@gmail.com>
2023-06-09 12:20:17 +0500

    changed multiple things

--- js/app.js
+++ js/app.js
@@ -1,46 +1,6 @@
-const formElement = document.getElementById("form");
-const errorElement = document.getElementById("error");
-formElement.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const nameElement = document.getElementById("name");
-  const emailElement = document.getElementById("email");
-  const passwordElement = document.getElementById("password");
-  const passwordConfirmationElement = document.getElementById(
-    "password_confirmation"
-  );
-  let nameValue = nameElement.value;
-  let emailValue = emailElement.value;
-  let passwordValue = passwordElement.value;
-  let passwordConfirmationValue = passwordConfirmationElement.value;
+const formElement = document.getElementById("form")
+const nameElement = document.getElementById("name")
+const emailElement = document.getElementById("email")
+const passwordElement = document.getElementById("password")
+const confirmPasswordElement = document.getElementById("password-confirmation")
 
-  function ErrorOut() {
-    errorElement.innerText = "";
-  }
-  nameElement.classList.remove("red-border");
-  emailElement.classList.remove("red-border");
-  passwordElement.classList.remove("red-border");
-  passwordConfirmationElement.classList.remove("red-border");
-  if (nameValue == "") {
-    errorElement.innerText = "Enter your name!";
-    nameElement.classList.add("red-border");
-  }
-  errorElement.innerText = "";
-
-  if (emailValue == "") {
-    errorElement.innerText = "Enter your email!";
-    emailElement.classList.add("red-border");
-  }
-  if (passwordValue == "") {
-    errorElement.innerText = "Enter your password!";
-    passwordElement.classList.add("red-border");
-  }
-  if (passwordValue != passwordConfirmationValue) {
-    errorElement.innerText = "Password does not match!";
-    passwordElement.classList.add("red-border");
-    passwordConfirmationElement.classList.add("red-border");
-  } else {
-    console.log("Good to go!");
-  }
-  // console.log(nameValue);
-  // console.log(nameElement);
-});
